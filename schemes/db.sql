cb71bd4dd3e171e663250dcb243ec40921e91334
crack81 <jgarnica@ant.com.mx>
2017-09-16 10:03:19 -0500

    fix data othe info private

--- schemes/db.sql
+++ schemes/db.sql
@@ -1,10 +1,10 @@
 create table usuario(id integer primary key auto_increment,
-				username varchar(45) not null,
-				email varchar(128) not null unique,
-                password varchar(30) not null,
-                tokencode varchar(255) not null unique);
+	username varchar(45) not null,
+	email varchar(128) not null unique,
+	password varchar(30) not null,
+	tokencode varchar(255) not null unique);
                 
                 
 
 insert into usuario(username,email,password,tokencode)
-	values ('remy','mauro@gmail.com','intelmexico','hashcode2');
+	values ('user1','test-email@gmail.com','password','tokencode');
